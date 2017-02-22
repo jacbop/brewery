50b6976f6c6b83fd323ed39e8836c6739cc99c32
Tom Wilberding <tom@wilberding.com>
2017-02-22 16:13:33 -0600

    update

--- src/main/java/com/wilberding/brewery/Volume.java
+++ src/main/java/com/wilberding/brewery/Volume.java
@@ -24,25 +24,33 @@ public class Volume {
             } else {
                 double observedVolume = Double.parseDouble(volString);
                 double observedTemp = Double.parseDouble(tempString);
-                double vol1 = actualVolume(observedVolume, observedTemp);
+                double vol0 = actualVolume(observedVolume, observedTemp);
                 context.out.println(
                         String.format(
                                 "Observed Volume %s @ %s F = Actual %s @ %s F",
-                                observedVolume, observedTemp, vol1, Constants.refTemp));
+                                observedVolume, observedTemp, vol0, Constants.refTemp));
             }
         }
     }
 
     static double actualVolume(double observedVolume, double observedTemp) {
         Curve curve = VolBetaCurve.INSTANCE.getCurve();
-        double vol2 = observedVolume;
-        double temp2 = CtoF.toC(observedTemp);
         double temp1 = CtoF.toC(Constants.refTemp);
-        double beta2 = curve.y(temp2);
-        double beta1 = curve.y(temp1);
-        // double vol1 = vol2 + (vol2 * ((beta1 + beta2) * 0.5) * (temp1 - temp2));
-        double vol1 = vol2 + (vol2 * beta2 * (temp1 - temp2));
+        double temp0 =  CtoF.toC(observedTemp);
+        double vol0 = observedVolume;
+        double beta0 = curve.y(temp0);
+        double vol1 = vol0 + (vol0 * beta0 * (temp1 - temp0));
         return vol1;
+    }
+
+    static void integrate(double temp0, double temp1, double vol0) {
+        boolean heating = temp0 < temp1;
+        double t0 = temp0;
+        double t1 = (heating) ? Math.min(temp0 + 1.0, temp1) : Math.max(temp0 - 1.0, temp1);
+        double v0 = vol0;
+        do {
+            System.out.println();
+        } while (t1 < temp1);
     }
 
 }
