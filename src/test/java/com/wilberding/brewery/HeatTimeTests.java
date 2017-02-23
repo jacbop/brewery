74c2152f347e43633e34fbbbf18e4ef8e4b1f69a
Tom Wilberding <tom@wilberding.com>
2017-02-23 10:16:02 -0600

    heat time

--- src/test/java/com/wilberding/brewery/HeatTimeTests.java
+++ src/test/java/com/wilberding/brewery/HeatTimeTests.java
@@ -13,8 +13,8 @@ public class HeatTimeTests {
 
     @Test
     public void basics() {
-        assertThat(HeatTime.heatTime(32.0, 212.0)).isEqualTo(100.0);
-        assertThat(HeatTime.heatTime(55.0, 165.0)).isEqualTo(62.341605839416054);
-        assertThat(HeatTime.heatTime(141.0, 212.0)).isEqualTo(38.60948905109489);
+        assertThat(HeatTime.heatTime(32.0, 212.0)).isEqualTo(96.0);
+        assertThat(HeatTime.heatTime(55.0, 165.0)).isEqualTo(55.24749163879599);
+        assertThat(HeatTime.heatTime(141.0, 212.0)).isEqualTo(41.7);
     }
 }
--- src/test/java/com/wilberding/brewery/data/HeatTimeCurveTest.java
+++ src/test/java/com/wilberding/brewery/data/HeatTimeCurveTest.java
@@ -15,10 +15,10 @@ public class HeatTimeCurveTest {
     @Test
     public void testBasics() {
         Curve sut = HeatTimeCurve.INSTANCE.getCurve();
-        assertThat(sut.y(50.0)).isEqualTo(9.469565217391304);
-        assertThat(sut.y(100.0)).isEqualTo(39.09489051094891);
-        assertThat(sut.y(149.0)).isEqualTo(65.74087591240877);
-        assertThat(sut.y(150.0)).isEqualTo(66.28467153284672);
-        assertThat(sut.y(212.0)).isEqualTo(100.0);
+        assertThat(sut.y(50.0)).isEqualTo(37.8);
+        assertThat(sut.y(100.0)).isEqualTo(62.8125);
+        assertThat(sut.y(149.0)).isEqualTo(87.46428571428571);
+        assertThat(sut.y(150.0)).isEqualTo(88.0);
+        assertThat(sut.y(212.0)).isEqualTo(125.0);
     }
 }
